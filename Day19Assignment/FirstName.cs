0b79700af4dd4929af4e5f11a58ba80a67facea8
hkvijay54 <hkvijay54@gmail.com>
2022-01-11 23:22:26 +0530

    uc2

--- Day19Assignment/FirstName.cs
+++ Day19Assignment/FirstName.cs
@@ -9,6 +9,7 @@ namespace Day19Assignment
     public class FirstName
     {
         public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
         public void FirstNameRegex(string first_name)
         {
             if(Regex.IsMatch(first_name, REGEX_FIRST_NAME))
@@ -18,6 +19,18 @@ namespace Day19Assignment
             else
             {
                 Console.WriteLine("FirstName is not valid");
+            }
+        }
+
+        public void LastNameRegex(string last_name)
+        {
+            if (Regex.IsMatch(last_name, LAST_NAME_REGEX))
+            {
+                Console.WriteLine("LastName is Valid");
+            }
+            else
+            {
+                Console.WriteLine("LastName is not valid");
             }
         }
 
--- Day19Assignment/Program.cs
+++ Day19Assignment/Program.cs
@@ -10,11 +10,19 @@ namespace Day19Assignment
     {
         public static void Main(string[] args)
         {
-            FirstName fn = new FirstName(); ;
+            FirstName fn = new FirstName();
+
             Console.Write("Enter your first name : ");
             var check = Console.ReadLine();
+            Console.Write("Enter your Last name : ");
+            var lastcheck = Console.ReadLine();
+
             fn.FirstNameRegex(check);
             Console.WriteLine(check);
+
+            
+            fn.LastNameRegex(lastcheck);
+            Console.WriteLine(lastcheck);
         }
     }
 }
