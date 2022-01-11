4ea68ae7d304c3b6740a5c8901ca22a1744517ed
hkvijay54 <hkvijay54@gmail.com>
2022-01-11 23:08:57 +0530

    uc1

---
+++ Day19Assignment/FirstName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19Assignment
+{
+    public class FirstName
+    {
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void FirstNameRegex(string first_name)
+        {
+            if(Regex.IsMatch(first_name, REGEX_FIRST_NAME))
+            {
+                Console.WriteLine("Firstname is Valid");
+            }
+            else
+            {
+                Console.WriteLine("FirstName is not valid");
+            }
+        }
+
+    }
+}
--- Day19Assignment/Program.cs
+++ Day19Assignment/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Regex Assignment");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FirstName fn = new FirstName(); ;
+            Console.Write("Enter your first name : ");
+            var check = Console.ReadLine();
+            fn.FirstNameRegex(check);
+            Console.WriteLine(check);
+        }
+    }
+}
