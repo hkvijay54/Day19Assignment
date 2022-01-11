25524c0e1231ceaea2e90ca37562631929f724c6
hkvijay54 <hkvijay54@gmail.com>
2022-01-12 00:41:59 +0530

    uc5

---
+++ Day19Assignment/Password.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19Assignment
+{
+    public class Password
+    {
+        public static string REGEX_PASSWORD_RULE1 = "^[A-Z a-z]{8,}$";
+
+        public void Passwordrule1(string pass1)
+        {
+            if (Regex.IsMatch(pass1, REGEX_PASSWORD_RULE1))
+            {
+                Console.WriteLine("Mobile Number is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Mobile Number is not valid");
+            }
+        }
+    }
+}
